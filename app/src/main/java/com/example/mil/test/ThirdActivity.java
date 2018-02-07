b5b9ac9edb37714801af96d984aa8e9e42e40baa
Nattikarn <nattikarn@owndays-tech.com>
2018-02-07 15:10:22 +0700

    created activity 3

--- app/src/main/java/com/example/mil/test/ThirdActivity.java
+++ app/src/main/java/com/example/mil/test/ThirdActivity.java
@@ -11,20 +11,25 @@ import android.widget.Button;
  * Created by mil on 2/7/2018 AD.
  */
 
-public class ThirdActivity extends AppCompatActivity {
+public class ThirdActivity extends MainActivity {
 
     private Button thirdBtn;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
 
         thirdBtn = findViewById(R.id.thirdBtn);
+
+        Intent intent = getIntent();
+        String txt = intent.getStringExtra("typeSthValue");
+
 
         thirdBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(ThirdActivity.this, Test.class);
+                Intent intent = new Intent(ThirdActivity.this, MainActivity.class);
                 startActivity(intent);
             }
         });
