866ed0b3231e51a250e7ab3375257b516cbb4fa6
Nattikarn <nattikarn@owndays-tech.com>
2018-02-07 10:08:14 +0700

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/res/layout/activity_test.xml

--- app/src/main/java/com/example/mil/test/Test.java
+++ app/src/main/java/com/example/mil/test/Test.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -28,11 +29,14 @@ public class Test extends AppCompatActivity {
 
         Intent intent = getIntent();
         String txt = intent.getStringExtra("typeSthValue");
+        showTextActivity2.setText(txt);
 
         btnActivity2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Log.d("haha","Activity2 Button");
+                Intent intent = new Intent(Test.this, MainActivity.class);
+                startActivity(intent);
             }
         });
     }
