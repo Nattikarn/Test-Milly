dd895a616a6fc944795b75ca561f0065e729247d
Nattikarn <nattikarn@owndays-tech.com>
2018-02-08 17:11:03 +0700

    Edited Third Activity UI + etc.

--- app/src/main/java/com/example/mil/test/ThirdActivity.java
+++ app/src/main/java/com/example/mil/test/ThirdActivity.java
@@ -6,6 +6,8 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 /**
  * Created by mil on 2/7/2018 AD.
@@ -13,24 +15,27 @@ import android.widget.Button;
 
 public class ThirdActivity extends MainActivity {
 
+    private ImageView picThirdAct;
     private Button thirdBtn;
+    private EditText editTextType1, editTextType2;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
 
+        picThirdAct = findViewById(R.id.picThirdAct);
+        picThirdAct = findViewById(R.id.picThirdAct);
+        editTextType1 = findViewById(R.id.editTextType1);
+        editTextType2 = findViewById(R.id.editTextType2);
         thirdBtn = findViewById(R.id.thirdBtn);
-
-        Intent intent = getIntent();
-        String txt = intent.getStringExtra("typeSthValue");
-
 
         thirdBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(ThirdActivity.this, MainActivity.class);
-                startActivity(intent);
+                Intent intent = getIntent();
+                String txt = intent.getStringExtra("typeSthValue");
+
             }
         });
     }
